3f58ce9f62a8a5e33e5e15bbb5e15bbbddbc0e49
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-04 22:18:58 +0100

    connexion to mongdb ok

---
+++ mongo.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.MONGO_URL);
+
+async function main() {
+    await client.connect();
+    console.log("Connexion ok !");
+    return "done!";
+}
+
+main().then(console.log).catch(console.error).finally(() => client.close());
