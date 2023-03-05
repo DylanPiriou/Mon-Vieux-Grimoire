4df9e697acaadbcc1b0f40f98dbf2d7083f99af8
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-05 17:43:22 +0100

    logic for getBooks and getBook done

--- controllers/books.controller.js
+++ controllers/books.controller.js
@@ -1,16 +1,35 @@
+const booksModel = require("../models/books.model");
 const BooksModel = require("../models/books.model");
+
+// -------- CREATION DE LA LOGIQUE DU CRUD --------- //
 
 // Récupérer tous les livres dans la base de données
 module.exports.getBooks = async (req, res) => {
-    res.json({ message : "les livres ont été récupérés avec succès !"})
+    try{
+        const books = await booksModel.find();
+        res.status(200).json(books);
+    }
+    catch(error){
+        res.status(400).json({ error : "Une erreur est survenue. Impossible de récupérer les livres."})
+    }
 }
 // Récupérer un livre en fonction de son id
 module.exports.getBook = async (req, res) => {
-    res.json({ message : "le livre a été récupéré avec succès !"})
+    try{
+        const book = await booksModel.findOne({_id: req.params.id});
+        res.status(200).json(book);
+    }
+    catch(error){
+        res.status(400).json({ error : `Impossible de récupérer le livre n°${req.params.id}`})
+    }
 }
 // Récupérer les 3 livres avec la meilleure note moyenne
 module.exports.getTopBooks = async (req, res) => {
-    res.json({ message : "les 3 meilleurs livres ont été récupérés avec succès !"})
+    try{
+    }
+    catch(error){
+        res.status(400).json({ error : "Une erreur est survenue."})
+    }
 }
 // Ajouter un livre
 module.exports.createBook = async (req, res) => {
