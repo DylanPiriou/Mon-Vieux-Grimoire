8d0d1cb66d69f7406718f9f59a53a762f4126dfa
DylanPiriou <dylan.piriou@hotmail.com>
2023-03-08 17:20:46 +0100

    deletebook controller ok

--- backend/controllers/books.controller.js
+++ backend/controllers/books.controller.js
@@ -1,37 +1,38 @@
 const booksModel = require("../models/books.model");
+const fs = require("fs");
 
 
 // -------- CREATION DE LA LOGIQUE DU CRUD POUR LES LIVRES --------- //
 
 // Récupérer tous les livres dans la base de données
 module.exports.getBooks = async (req, res) => {
-    try{
+    try {
         const books = await booksModel.find();
         res.status(200).json(books);
     }
-    catch(error){
-        res.status(400).json({ error: process.env.DEV_MODE === "dev" ? error : "Une erreur est survenue. Impossible de récupérer les livres."})
+    catch (error) {
+        res.status(400).json({ error: process.env.DEV_MODE === "dev" ? error : "Une erreur est survenue. Impossible de récupérer les livres." })
     }
 }
 // Récupérer les 3 livres avec la meilleure note moyenne
 module.exports.getTopBooks = async (req, res) => {
-    try{
+    try {
         const topBooks = await booksModel.find().sort({ averageRating: -1 }).limit(3);
         res.status(200).json(topBooks);
     }
-    catch(error){
-        res.status(400).json({ error: process.env.DEV_MODE === "dev" ? error : "Une erreur est survenue."})
+    catch (error) {
+        res.status(400).json({ error: process.env.DEV_MODE === "dev" ? error : "Une erreur est survenue." })
     }
 }
 // Récupérer un livre en fonction de son id
 module.exports.getBook = async (req, res) => {
     const bookId = req.params.id;
-    try{
+    try {
         const book = await booksModel.findOne({ _id: bookId });
         res.status(200).json(book);
     }
-    catch(error){
-        res.status(400).json({ error: process.env.DEV_MODE === "dev" ? error : `Impossible de récupérer le livre n°${bookId}`})
+    catch (error) {
+        res.status(400).json({ error: process.env.DEV_MODE === "dev" ? error : `Impossible de récupérer le livre n°${bookId}` })
     }
 }
 // Ajouter un livre
@@ -44,44 +45,55 @@ module.exports.createBook = (req, res) => {
         ...bookObject,
         userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-        });
+    });
     book.save()
-    .then(() => res.status(201).json({ message: "Livre enregistré."}))
-    .catch(error => res.status(400).json({ error }))
+        .then(() => res.status(201).json({ message: "Livre enregistré." }))
+        .catch(error => res.status(400).json({ error }))
 }
 
 // Modifier un livre
 module.exports.updateBook = (req, res) => {
-    // Vérifie si il y a un fichier
+    // Vérifie si il y a un fichier, si oui on traite l'image sinon on traite l'objet
     const bookObject = req.file ? {
         ...JSON.parse(req.body.book),
         imageUrl: `${req.protocol}://${req.get("host")}/images/${req.file.filename}`
-    } : { ...req.body};
+    } : { ...req.body };
     delete bookObject._userId;
     // Recherche le livre en fonction de l'id dans la base de données
     booksModel.findOne({ _id: req.params.id })
-    .then(book => {
-        if(book.userId !== req.auth.userId){
-            res.status(401).json({ message: "Non autorisé à modifier ce livre." });
-        } else {
-            booksModel.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
-            .then(() => res.status(201).json({ message: "Le livre a été modifié avec succès !" }))
-            .catch(error => res.status(401).json({ error }));
-        }
-    })
+        .then(book => {
+            // Vérifie si l'userId du livre correspond à celui qui fait la requête
+            if (book.userId !== req.auth.userId) {
+                res.status(401).json({ message: "Non autorisé à modifier ce livre." });
+            } else {
+                // Modifier les données du livre dans la base de données
+                booksModel.updateOne({ _id: req.params.id }, { ...bookObject, _id: req.params.id })
+                    .then(() => res.status(201).json({ message: "Le livre a été modifié avec succès !" }))
+                    .catch(error => res.status(401).json({ error }));
+            }
+        })
 }
 // Supprimer un livre
-module.exports.deleteBook = async (req, res) => {
-    const bookId = req.params.id;
-    try{
-        const book = await booksModel.findOne(bookId);
-        const userId = await booksModel.userId;
-    }
-    catch{
-
-    }
-}
+module.exports.deleteBook = (req, res) => {
+    // Recherche 'lid du livre à supprimer dans la base de données
+    booksModel.findOne({ _id: req.params.id })
+        .then(book => {
+            // Vérifie si l'userId du livre correspond à celui qui fait la requête
+            if (book.userId !== req.auth.userId) {
+                res.status(401).json({ message: "Non autorisé à supprimer ce livre." });
+            } else {
+                // Récupère le nom du fichier
+                const filename = book.imageUrl.split("/images/")[1];
+                fs.unlink(`images/${filename}`, () => {
+                    // Supprime le livre de la base de données
+                    booksModel.deleteOne({ _id: req.params.id })
+                        .then(() => res.status(201).json({ message: "Le livre a été supprimé avec succès !" }))
+                        .catch(error => res.status(401).json({ error }));
+                })
+            }
+        })
+};
 // Ajouter une note à un livre
 module.exports.addRating = async (req, res) => {
-    res.json({ message : "le livre a été noté avec succès !"})
+    res.json({ message: "le livre a été noté avec succès !" })
 }
